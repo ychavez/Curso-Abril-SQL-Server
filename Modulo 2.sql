7d992cb04bc40d6362212af04f29affce8126c6d
Yael Chavez <you400@gmail.com>
2023-05-29 20:10:05 -0600

    modulo 2

--- Modulo 2.sql
+++ Modulo 2.sql
@@ -103,3 +103,62 @@ select * from Empleado where AMaterno is null
 -- traeme todos los que Amaterno no sea null
 select * from Empleado where AMaterno is not null
 
+--- trame los que se llamen Juan y Omar
+select * from Empleado where (Nombre = 'Omar' or Nombre = 'juan') and AMaterno is not null
+
+-- trame todos los que se llamaen pedro, julio, Juan, Omar
+select * from Empleado where nombre in ('pedro',  'Juan', 'Omar')
+
+-- trame todos los que se no llamaen pedro, julio, Juan, Omar
+select * from Empleado where nombre not  in ('pedro',  'Juan', 'Omar')
+
+
+/*
+1.- insertar 10 producto
+2.- aplicar la logica del like que inicie con una letra
+3.- que termine con una letra
+4.- contenga una letra
+5.- que la tercer letra sea x
+6.- que la primera letra este dentro de un rango ejemplo 'A a la J'
+7.- consultar aquellas que esten dentro de un nombre 
+8.- consultar aquellas que no esten dentro de un nombre
+*/
+
+
+create table Puesto
+(
+Id int not null,
+Nombre varchar(40) not null
+)
+
+
+insert into Puesto values (1, 'Desarrollador')
+insert into Puesto values (2, 'PM')
+insert into Puesto values (3, 'PO')
+
+-- alter nos va a servir para modificar algun elemento de nuestra base de datos como Tabla, Vista, SP
+alter table puesto
+-- definimos una regla en la cual el Id sera llave primaria
+add constraint PK_Puesto Primary key (id)
+
+-- drop table elimina la tabla
+drop table Empleado
+go
+create table Empleado
+(
+    NumeroEmpleado int primary key identity(1,1),
+	Nombre varchar(50) not null,
+	APaterno varchar(50) not null,
+	AMaterno varchar(50) null,
+	PuestoId int foreign key references Puesto(id)
+)
+
+
+
+
+select * from Puesto
+
+
+select * from Empleado
+
+
